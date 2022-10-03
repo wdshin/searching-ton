2828464a7cf93a35940d36dede01b010a4d73cdb
matthew <matthew@matthews-MacBook-Pro.local>
2022-10-03 23:37:20 +0400

    fix urls

--- searching-front/app/core/helpers/common.ts
+++ searching-front/app/core/helpers/common.ts
@@ -2,7 +2,7 @@ import classnames from "classnames"
 export const cn = classnames
 
 export const cleanUrlForUi = (url: string) => {
-  return url.replace("https://", "").replace("http://", "").replace("/", "")
+  return url.replace("https://", "").replace("http://", "")
 }
 
 export const isNode = () => {
