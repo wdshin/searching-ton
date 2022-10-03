6b3ab3659197c1e15e969ab0143804e6ab963f81
matthew <matthew@matthews-MacBook-Pro.local>
2022-10-03 21:38:33 +0400

    fix auth cookies

--- searching-front/app/blitz-client.ts
+++ searching-front/app/blitz-client.ts
@@ -1,11 +1,17 @@
-import { AuthClientPlugin } from "@blitzjs/auth"
+// import { AuthClientPlugin } from "@blitzjs/auth"
 import { setupBlitzClient } from "@blitzjs/next"
 import { BlitzRpcPlugin } from "@blitzjs/rpc"
+
+const authStub = ()=>{
+  globalThis.__BLITZ_SESSION_COOKIE_PREFIX = "blitz";
+  return {events:{},middleware:{}, exports: ()=>({})}
+}
 
 // export const authConfig = {
 //   cookiePrefix: "searching-front-cookie-prefix",
 // }
 //AuthClientPlugin(authConfig)
 export const { withBlitz } = setupBlitzClient({
-  plugins: [BlitzRpcPlugin({})],
+  plugins: [authStub(),BlitzRpcPlugin({})],
 })
+
