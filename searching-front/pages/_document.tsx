8080287c3877402ab4545fce505cb0e5fe70447b
matthew <matthew@matthews-MacBook-Pro.local>
2022-10-03 23:50:06 +0400

    add metrics

--- searching-front/pages/_document.tsx
+++ searching-front/pages/_document.tsx
@@ -12,6 +12,22 @@ class MyDocument extends Document {
         <Head />
         <body>
           <Main />
+<script type="text/javascript" dangerouslySetInnerHTML={{__html: `
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(90644479, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });`}} >
+  
+</script>
+
+
           <NextScript />
           <div id="modal-root"></div>
         </body>
