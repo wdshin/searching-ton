c587b2e75717b1444b4ebdd462cb584c3b3f64e5
matthew <matthew@matthews-MacBook-Pro.local>
2022-10-08 16:42:58 +0400

    add query param to websites

--- searching-front/app/core/components/WebsiteCard/WebsiteCard.tsx
+++ searching-front/app/core/components/WebsiteCard/WebsiteCard.tsx
@@ -7,12 +7,16 @@ interface Props {
   description: string
 }
 const WebsiteCard = (props: Props) => {
+  const urlObj = new URL(props.url)
+  urlObj.searchParams.set("from", "Searching.ton")
+
+  const url = urlObj.toString();
   return (
     <div className={s.root}>
-      <a target="_blank" className={s.titleLink} href={props.url}>
+      <a target="_blank" className={s.titleLink} href={url}>
         {props.title}
       </a>
-      <a target="_blank" className={s.miniLink} href={props.url}>
+      <a target="_blank" className={s.miniLink} href={url}>
         {cleanUrlForUi(props.url)}
       </a>
       <div className={s.description}>{props.description}</div>
