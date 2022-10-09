8de8b8b65b3c750dd1b4cd2bd60d6fb28c165e95
matthew <matthew@matthews-MacBook-Pro.local>
2022-10-09 13:30:37 +0400

    add try catch html to text

--- searching-front/services/modules/parser/helpers.ts
+++ searching-front/services/modules/parser/helpers.ts
@@ -12,12 +12,16 @@ const convert = (html: string) =>
     listStyle: "linebreak",
   })
 export const htmlToText = (html: string) => {
-  const text = convert(html)
-
-  return text
-    .replaceAll(/&\w*;/g, "")
-    .replaceAll(/<!--\s?(.|\R|\s)*?-->/g, "")
-    .replaceAll(/<.*><\/.*>/g, "")
+  try {
+    const text = convert(html)
+    return text
+      .replaceAll(/&\w*;/g, "")
+      .replaceAll(/<!--\s?(.|\R|\s)*?-->/g, "")
+      .replaceAll(/<.*><\/.*>/g, "")
+  } catch (e) {
+    console.log("htmlToText", e)
+    return ""
+  }
 }
 
 export const SHOULD_NOT_PARSE = "SHOULD_NOT_PARSE"
