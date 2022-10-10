39545ac8a5fb9e9c32e6700e632315559a721efe
matthew <matthew@matthews-MacBook-Pro.local>
2022-10-10 18:24:01 +0400

    add dev mode

--- searching-front/app/core/helpers/metrika.ts
+++ searching-front/app/core/helpers/metrika.ts
@@ -1,4 +1,10 @@
-export const count = (event:string) => {
-    yaCounter90644479.reachGoal(event)
-    gtag('event', event);
+import cookies from "js-cookie"
+
+export const count = (event: string) => {
+  const isDev = cookies.get("dev")
+  if (isDev) {
+    return
+  }
+  yaCounter90644479.reachGoal(event)
+  gtag("event", event)
 }
